c902312cdf4e4b54f39a4d59dfe6226208a7fc3b
shubham32sk <shubha3m@gmail.com>
2024-03-02 23:08:57 +0530

    initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,12 +7,17 @@ import androidx.appcompat.app.AppCompatActivity
 import net.objecthunter.exp4j.ExpressionBuilder
 
 
+
 class MainActivity : AppCompatActivity() {
-    private val tvResult:TextView=findViewById(R.id.tvResult)
-    private val tvExpression:TextView=findViewById(R.id.tvExpression)
+    lateinit var tvResult:TextView
+    lateinit var tvExpression:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //
+
+        tvResult=findViewById(R.id.tvResult)
+        tvExpression=findViewById(R.id.tvExpression)
         val tvOne:TextView=findViewById(R.id.tvOne)
         val tvTwo:TextView=findViewById(R.id.tvTwo)
         val tvThree:TextView=findViewById(R.id.tvThree)
@@ -29,55 +34,66 @@ class MainActivity : AppCompatActivity() {
         val tvMul:TextView=findViewById(R.id.tvMul)
         val tvDivide:TextView=findViewById(R.id.tvDivide)
         val tvOpen:TextView=findViewById(R.id.tvOpen)
-      //  val tvClose:TextView=findViewById(R.id.tvClose)
+        val tvClose:TextView=findViewById(R.id.tvClose)
         val tvClear:TextView=findViewById(R.id.tvClear)
         val tvEquals:TextView=findViewById(R.id.tvEquals)
+        val tvback:TextView=findViewById(R.id.tvBack)
 
         //Numbers
-        tvOne.setOnClickListener { appendOnExpression("1", true) }
-        tvTwo.setOnClickListener { appendOnExpression("2", true) }
-        tvThree.setOnClickListener { appendOnExpression("3", true) }
-        tvFour.setOnClickListener { appendOnExpression("4", true) }
-        tvFive.setOnClickListener { appendOnExpression("5", true) }
-        tvSix.setOnClickListener { appendOnExpression("6", true) }
-        tvSeven.setOnClickListener { appendOnExpression("7", true) }
-        tvEight.setOnClickListener { appendOnExpression("8", true) }
-        tvNine.setOnClickListener { appendOnExpression("9", true) }
-        tvZero.setOnClickListener { appendOnExpression("0", true) }
-        tvDot.setOnClickListener { appendOnExpression(".", true) }
+        tvOne.setOnClickListener { appendOnExpression("1",true) }
+        tvTwo.setOnClickListener { appendOnExpression("2",true) }
+        tvThree.setOnClickListener { appendOnExpression("3",true) }
+        tvFour.setOnClickListener { appendOnExpression("4",true) }
+        tvFive.setOnClickListener { appendOnExpression("5",true) }
+        tvSix.setOnClickListener { appendOnExpression("6",true) }
+        tvSeven.setOnClickListener { appendOnExpression("7",true) }
+        tvEight.setOnClickListener { appendOnExpression("8",true) }
+        tvNine.setOnClickListener { appendOnExpression("9",true) }
+        tvZero.setOnClickListener { appendOnExpression("0",true) }
+        tvDot.setOnClickListener { appendOnExpression(".",true) }
 
         //Operators
-        tvPlus.setOnClickListener { appendOnExpression("+", false) }
-        tvMinus.setOnClickListener { appendOnExpression("-", false) }
-        tvMul.setOnClickListener { appendOnExpression("*", false) }
-        tvDivide.setOnClickListener { appendOnExpression("/", false) }
-        tvOpen.setOnClickListener { appendOnExpression("(", false) }
-      //  tvClose.setOnClickListener { appendOnExpression(")", false) }
+        tvPlus.setOnClickListener { appendOnExpression("+",false) }
+        tvMinus.setOnClickListener { appendOnExpression("-",false) }
+        tvMul.setOnClickListener { appendOnExpression("*",false) }
+        tvDivide.setOnClickListener { appendOnExpression("/",false) }
+        tvOpen.setOnClickListener { appendOnExpression("(",false) }
+        tvClose.setOnClickListener { appendOnExpression(")",false) }
 
         tvClear.setOnClickListener {
             tvExpression.text = ""
             tvResult.text = ""
         }
+        tvback.setOnClickListener{
+            if(tvExpression.text.isNotEmpty()){
+               // tvExpression.text=tvExpression.text.substring(0,(tvExpression.text.toString().length)-1)
+                val new=tvExpression.text.dropLast(1)
+                tvExpression.text=new
+            }
+        }
 
         tvEquals.setOnClickListener {
-            try {
+            try{
+
 
                 val expression = ExpressionBuilder(tvExpression.text.toString()).build()
                 val result = expression.evaluate()
-                val longResult = result.toLong()
-                if(result == longResult.toDouble())
-                    tvResult.text = longResult.toString()
-                else
-                    tvResult.text = result.toString()
+                val longresult=result.toLong()
 
-            }catch (e:Exception){
-                Log.d("Exception"," message : " + e.message )
+            if(result==longresult.toDouble()){
+                tvResult.text=longresult.toString()
             }
+            else
+                tvResult.text=result.toString()
+            }catch (e:Exception){
+                Log.d("Exception","Message:"+e.message)
+            }
+
         }
 
     }
 
-    private fun appendOnExpression(string: String, canClear: Boolean) {
+    private fun appendOnExpression(string: String,canClear:Boolean) {
 
         if(tvResult.text.isNotEmpty()){
             tvExpression.text = ""
